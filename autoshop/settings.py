d9a6216bc695710cd51d195cc5f08ba0bcfb69f8
ssantamars <ssantamars@eafit.edu.co>
2026-04-13 19:14:21 -0500

    allowed hosts

--- autoshop/settings.py
+++ autoshop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-**97*y)fo@zbvq%$-o(&kvzmlq+c=a@grp(l3a@#dz+647_gb6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
